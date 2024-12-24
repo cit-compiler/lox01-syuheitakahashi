cf18dde97df8d5c22f35d50447361ea5d61e8834
syuheitakahashi <cisu22032@g.nihon-u.ac.jp>
2024-12-24 11:22:45 +0900

    add RuntimeError.java Interpreter.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 //import java.util.Scanner;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -26,6 +28,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -51,7 +54,9 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
+        //System.out.println(new AstPrinter().print(expression));
+        
         /*for (Token token : tokens) {
             System.out.println(token);
         }*/
@@ -75,5 +80,12 @@ public class Lox {
         } else {
           report(token.line, " at '" + token.lexeme + "'", message);
         }
-      }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
 }
